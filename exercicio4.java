6733e83c4d12b489b74f1c2a57eb43361686bf56
Rodrigo Viana <rodrigoviana50@gmail.com>
2020-08-17 15:47:16 -0300

    Corrigindo exercicio4

--- exercicio4.java
+++ exercicio4.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class exercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe sua idade");
+        int idade = scanner.nextInt();
+        System.out.println("informe sua altura");
+        int altura = scanner.nextInt();
 
         int cont = 0;
 
@@ -11,7 +15,20 @@ public class exercicio4 {
             int idade = scanner.nextInt();
             System.out.println("informe sua altura");
             int altura = scanner.nextInt();
-            
+            int media = 0;
+            int soma = 0;
+            if (altura < 1.7) {
+                soma = soma + idade;
+                        media = soma/cont;
+                System.out.println("A idade média das pessoas com menos de 1.7 é: " + media);
+            }
+                else if (idade > 20) {
+                    soma = soma + altura;
+                        media = soma/cont;
+                System.out.println("A altura média das pessoas com mais de 20 anos é: " + media);
+            }
+
+            cont++
         }
 
     }
